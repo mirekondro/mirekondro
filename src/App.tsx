5c12052af7d132e1a0f32bf79a4ff5cc4b515c21
mirekondro1 <130686217+mirekondro@users.noreply.github.com>
2025-09-02 04:14:23 +0200

    add: Social icons, edit: Font

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { FaGithub, FaLinkedin, FaXTwitter, FaInstagram } from 'react-icons/fa6'
 import './App.css'
 
 function ClickableBlock({ logo, title, subtitle, subtitle2, onClick }) {
@@ -14,17 +15,30 @@ function ClickableBlock({ logo, title, subtitle, subtitle2, onClick }) {
   )
 }
 
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div style={{ maxWidth: '800px', margin: '0 auto', padding: '32px' }}>
+
+        <div className="social-icons">
+          <a href="https://github.com/mirekondro" target="_blank" rel="noopener noreferrer">
+            <FaGithub size={20} />
+          </a>
+          <a href="https://linkedin.com/in/mirekondro" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin size={20} />
+          </a>
+          <a href="https://x.com/leaftys" target="_blank" rel="noopener noreferrer">
+            <FaXTwitter size={20} />
+          </a>
+          <a href="https://instagram.com/mirekondro" target="_blank" rel="noopener noreferrer">
+            <FaInstagram size={20} />
+          </a>
+        </div>
+
         <h1 className='name'>Miroslav Ondroušek</h1>
-
         <h2>Software and Web Developer & Designer</h2>
-
         <p>
           I create modern web apps and interfaces that combine smart coding with thoughtful design, turning ideas into seamless digital experiences.
         </p>
@@ -37,14 +51,12 @@ function App() {
             subtitle="Website and visual identity for a roofing company."
             onClick={() => window.open("https://weroof.cz", "_blank")}
           />
-
           <ClickableBlock
             logo="/feedy.png"
             title="Feedy - Food Delivery App"
             subtitle="Final school project. Website and mobile app for food delivery service."
             onClick={() => window.open("https://feedy-three.vercel.app/", "_blank")}
           />
-
           <ClickableBlock
             logo="/lastel.png"
             title="LASTEL POTS s.r.o."
@@ -54,37 +66,35 @@ function App() {
         </div>
 
         <h1 className='work'>Work experience</h1>
-          <div className='experience-list'>
-            <ClickableBlock
-              logo="/blogic.png"
-              title="Business Logic s.r.o."
-              subtitle="Intership 2023 - Fullstack Developer (C# .NET MVC)"
-              subtitle2="Intership 2024 - Mobile App Developer (React Native)"
-              onClick={() => window.open("https://www.blogic.cz/", "_blank")}
-            />
+        <div className='experience-list'>
+          <ClickableBlock
+            logo="/blogic.png"
+            title="Business Logic s.r.o."
+            subtitle="Intership 2023 - Fullstack Developer (C# .NET MVC)"
+            subtitle2="Intership 2024 - Mobile App Developer (React Native)"
+            onClick={() => window.open("https://www.blogic.cz/", "_blank")}
+          />
+          <ClickableBlock
+            logo="/zff.jpg"
+            title="Zlín Film Festival"
+            subtitle="Volunteer 2022, Virtual Reality Department"
+            onClick={() => window.open("https://www.zlinfest.cz/", "_blank")}
+          />
+        </div>
 
-            <ClickableBlock
-              logo="/zff.jpg"
-              title="Zlín Film Festival"
-              subtitle="Volunteer 2022, Virtual Reality Department"
-              onClick={() => window.open("https://www.zlinfest.cz/", "_blank")}
-            />
-          </div>
-
-          <h1 className='contact'>Contact</h1>
-          <p>
-  Got a project in mind or want to collaborate? Feel free to reach out – I’d love to hear from you! 
-  You can email me at 
-  <a href="mailto:mirekondro@post.cz"> mirekondro@post.cz </a>
-  or call me at 
-  <a href="tel:+420604164894"> +420 604 164 894</a> (🇨🇿)
-  or 
-  <a href="tel:+4531814408"> +45 31814408</a> (🇩🇰).
-</p>
-
+        <h1 className='contact'>Contact</h1>
+        <p>
+          Got a project in mind or want to collaborate? Feel free to reach out – I’d love to hear from you! 
+          You can email me at 
+          <a href="mailto:mirekondro@post.cz"> mirekondro@post.cz </a>
+          or call me at 
+          <a href="tel:+420604164894"> +420 604 164 894</a> (🇨🇿)
+          or 
+          <a href="tel:+4531814408"> +45 31814408</a> (🇩🇰).
+        </p>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
